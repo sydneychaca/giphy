04ba058325870d44a321db6ea73625cc972bb476
sydneychaca <sydneychaca@gmail.com>
2021-07-19 19:03:48 +0000

    new

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
@@ -14,3 +14,15 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+# add route for your gif results
+@app.route('/yourgif', methods = ["GET","POST"])
+def yourgif(): #name here does not matter but usually name of route
+    #get the giph for giphy and puts the link on webpage
+    user_response = request.form["gifchoice"]
+    #user_response = "dog"
+    gif_link = getImageUrlFrom(user_response)
+    print(gif_link)
+    #pass URL to render template and have that URL appear as an image in yourgof.html
+    return render_template("yourgif.html", time = datetime.now(), gif_link = gif_link)
+    #add datetime.now() to trick our browser into updating the css
--- model.py
+++ model.py
@@ -1,2 +1,5 @@
+import requests
 def getImageUrlFrom(query):
-    pass
+    giphy_request_link = "https://api.giphy.com/v1/gifs/search?api_key=dIm2FEAJQKeOTxVk8bZSbQK2El4VZfUh&q=" + query + "&limit=25&offset=0&rating=g&lang=en"
+    response = requests.get(giphy_request_link).json()
+    return response["data"][0]["images"]["fixed_height"]["url"]
